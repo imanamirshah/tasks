ab0449db5cb725c113d75b99cf79fa60d072e136
imanamirshah <iman.amirshah@gmail.com>
2024-02-23 17:16:43 +0800

    Consume new methods in main

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -14,11 +14,13 @@ public class Main {
 
         System.out.println("Printing all data ...");
         printAllData(tasksData);
+        printDataUsingStream(tasksData);
 
         System.out.println("Printing deadlines ...");
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines: " + countDeadlinesUsingStream(tasksData));
 
     }
 
